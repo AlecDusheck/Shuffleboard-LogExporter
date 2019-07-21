c99e62abaf73a9e112b3fce03a1503ab7db79a16
AlecDusheck <a.dusheckirl@gmail.com>
2019-07-21 15:49:11 -0500

    Fix error with method names and locations

--- src/main/java/com/team2502/shuffleboard/logexporter/ExportLogWidget.java
+++ src/main/java/com/team2502/shuffleboard/logexporter/ExportLogWidget.java
@@ -4,13 +4,12 @@ import edu.wpi.first.shuffleboard.api.data.types.NoneType;
 import edu.wpi.first.shuffleboard.api.widget.Description;
 import edu.wpi.first.shuffleboard.api.widget.ParametrizedController;
 import edu.wpi.first.shuffleboard.api.widget.SimpleAnnotatedWidget;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
 
-import java.awt.event.ActionEvent;
-
 @Description(dataTypes = {NoneType.class}, name = "Export logs")
-@ParametrizedController(value = "ExportLogWidget.fxml")
+@ParametrizedController(value = "com.team2502.shuffleboard.logexporter/ExportLogWidget.fxml")
 public class ExportLogWidget extends SimpleAnnotatedWidget {
 
     public Pane getView() {
